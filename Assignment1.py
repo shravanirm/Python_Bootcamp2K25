48589be5f44cb378e69bb09b1d93ce1189a19c4d
Shravani Mokashi <shravanimokashi3star@gmail.com>
2025-07-22 22:11:34 +0530

    1st commit

---
+++ Assignment1.py
@@ -0,0 +1,30 @@
+
+print('Task 1')
+# Task 1: Print 5 different messages
+print("Welcome to Python programming!")  # M 1
+print("Python is high level programing language")
+print("hi my name is shravani.")  # M 3
+print("how are you?")  # M 4
+print("hello")  # M 5
+
+print('--------------------------------------------')
+# Task 2: Function that print name and age
+print('Task 2')
+def name_age():
+   
+    name = "Shravani Mokashi"
+    age = 20
+    print("Name:", name)
+    print("Age:", age)
+
+name_age()
+
+print('--------------------------------------------')
+# Task 5: Simple calculator to add two numbers
+print('Task 5')
+def calsi(num1, num2):
+    add = num1 + num2
+    print("Sum of", num1, "and", num2, "is:", add)
+
+calsi(10, 25)
+
